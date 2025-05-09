88cc03a8344e3773adcf9d3c0eb8e8772a9655d7
GauravJangra9988 <gjangra9988@gmail.com>
2025-05-09 09:08:06 +0530

    1

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ import { connectDB } from "../lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/",(req,res)=>{
+    res.json("Server Running");
+})
+
 app.use(express.json());
 app.use(cors());
 
